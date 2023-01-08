5a10d21f32741ac22c9b1a9b174345cf0fab2c8c
Poitier Stringer <stringerpoitier@gmail.com>
2023-01-08 17:31:34 +0000

    type main layout components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { red } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Container from '@mui/material/Container';
+import { TextField } from '@mui/material';
+import { useState } from 'react';
 
 const theme = createTheme({
   palette: {
@@ -12,6 +14,11 @@ const theme = createTheme({
   },
 });
 
+interface EditorProps {
+  editorState: string;
+  updateEditorState: (editorState: string) => void;
+}
+
 const Nav = () => {
   return (
     <div className='nav'>
@@ -20,38 +27,55 @@ const Nav = () => {
   );
 };
 
-const Editor = () => {
+const Editor = ({editorState, updateEditorState}: EditorProps) => {
+
   return (
     <div className='editor'>
       <h1>Editor</h1>
+      <TextField
+        id="outlined-multiline-static"
+        className='editor__input'
+        label="Multiline"
+        multiline
+        defaultValue={editorState}
+        onChange={(e) => updateEditorState(e.target.value)}
+        variant="outlined"
+        rows={80}
+        fullWidth
+      />
     </div>
   );
 };
 
-const Preview = () => {
+const Preview = ({editorState, updateEditorState}: Partial<EditorProps>) => {
   return (
     <div className='preview'>
       <h1>Preview</h1>
+      <div className='preview__output'>
+        {editorState}
+      </div>
     </div>
   );
 };
 
-const Main = () => {
+const Main = (editorProps: EditorProps) => {
+  const { editorState, updateEditorState } = editorProps;
   return (
     <div className='main'>
-      <Editor />
-      <Preview />
+      <Editor  editorState={editorState} updateEditorState={updateEditorState}/>
+      <Preview editorState={editorState}/>
     </div>
   );
 };
 
-
 function App() {
+  const [editorState, updateEditorState] = useState('');
+
   return (
     <ThemeProvider theme={theme ?? {}}>
     <Nav />
     <Container className="app">
-      <Main />
+      <Main editorState={editorState} updateEditorState={updateEditorState}/>
     </Container>
     </ThemeProvider>
   );
